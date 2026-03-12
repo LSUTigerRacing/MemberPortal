af245afac3155d430e23ba2ae7ddcc076f23ffdc
Ricky Liang <liangricky7@gmail.com>
2026-03-12 15:56:53 -0500

    feat (backend): update order models

--- backend/TRFSAE.MemberPortal.API/Models/OrderItem.cs
+++ backend/TRFSAE.MemberPortal.API/Models/OrderItem.cs
@@ -8,32 +8,32 @@ namespace TRFSAE.MemberPortal.API.Models;
 public class OrderItemModel : BaseModel
 {
     [PrimaryKey("id", false)]
-    public required Guid Id { get; set; }
+    public Guid Id { get; set; }
 
     [Column("orderId")]
-    public required Guid OrderId { get; set; }
+    public Guid OrderId { get; set; }
 
     [Column("name")]
-    public required string Name { get; set; }
+    public string Name { get; set; }
 
     [Column("partNumber")]
-    public required string PartNumber { get; set; }
+    public string PartNumber { get; set; }
 
     [Column("supplier")]
-    public required string Supplier { get; set; }
+    public string Supplier { get; set; }
 
     [Column("url")]
-    public required string URL { get; set; }
+    public string URL { get; set; }
 
     [Column("quantity")]
-    public required int Quantity { get; set; }
+    public int Quantity { get; set; }
 
     [Column("price")]
-    public required decimal Price { get; set; }
+    public decimal Price { get; set; }
 
     [Column("createdAt")]
-    public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     [Column("updatedAt")]
-    public required DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
