54e8639db07b663d69858b72a9e1130758adc31f
Damien Vesper <ldamienvesper@gmail.com>
2026-06-30 02:45:39 -0500

    fix (db): filter to only public

--- database/src/drizzle.config.ts
+++ database/src/drizzle.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    out: "./drizzle/dist",
-    schema: "./src/models",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!
     },
+    out: "./drizzle/dist",
+    schema: "./src/models",
+    schemaFilter: ["public"],
     strict: true,
     verbose: true
 });
